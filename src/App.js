bf0b97edd694fb57ca16e183114563c69abd83e0
Areeba <cmy61677@gmail.com>
2025-02-25 14:23:17 +0000

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Gitlab <code>CI/CD</code> pipeline for react-app
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning Gitlab CI/CD
         </a>
       </header>
     </div>
